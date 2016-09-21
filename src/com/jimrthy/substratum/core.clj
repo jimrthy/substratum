1993e38c7f021c8d5200c130be7371c7f224524d
James Gatannah <james.gatannah@gmail.com>
2016-09-21 04:56:36 +0000

    Converting core from schema to spec (WIP)
    First pass

--- src/com/jimrthy/substratum/core.clj
+++ src/com/jimrthy/substratum/core.clj
@@ -1,65 +1,223 @@
 (ns com.jimrthy.substratum.core
   "Core database functionality"
-  (:require [com.stuartsierra.component :as component]
-            [datomic.api :as d]
+  (:require [clojure.spec :as s]
             [com.jimrthy.substratum.util :as util]
+            [com.stuartsierra.component :as component]
+            [datomic.api :as d]
             [hara.event :refer [raise]]
-            [schema.core :as s]
             [taoensso.timbre :as log])
   (:import [datomic Datom]
            [datomic.db Db DbId]))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;; Schema
+;;; Specs
 ;;; N.B. Database schema definitions belong in admin.
 ;;; This is definitely for Prismatic-style schema and
 ;;; the way it interacts with Stuart Sierra Components
 
-(defn protocols []
+(s/def ::protocol
   ;; TODO: Add the rest
-  (s/enum :free :ram :sql))
+  #{::free ::ram ::sql})
 
-(defn sql-drivers
-  []
-  (s/enum :postgres))
+(s/def ::db-name string?)
+(s/def ::base-uri-description (s/keys :req [::db-name ::protocol]))
 
-(defn base-uri-description
-  []
-  {:db-name s/Str
-   :protocol (protocols)})
+(s/def ::in-memory-uri-description ::base-uri-description)
 
-(def in-memory-uri-description base-uri-description)
+(s/def ::port (s/and integer?
+                     (complement neg?)
+                     #(< % 65536)))
+(s/def ::describe-uri-with-port (s/merge ::base-uri-description
+                                         (s/keys ::port ::host)))
 
-(defn describe-uri-with-port
-  []
-  (assoc (base-uri-description)
-         :port s/Int
-         :host s/Str))
+(s/def ::free-uri-description ::describe-uri-with-port)
 
-(def free-uri-description describe-uri-with-port)
+(s/def ::sql-driver #{:postgres})
+(s/def ::user string?)
+(s/def ::password string?)
+;; This depends on the JDBC part of the connection.
+(s/def ::sql-uri-description
+  (s/merge ::describe-uri-with-port
+           (s/keys :opt [::sql-driver ::user ::password])))
 
-(defn sql-uri-description
-  "This all depends on the JDBC part of the connection."
-  []
-  (into (describe-uri-with-port)
-        {(s/optional-key :driver) (sql-drivers)
-         (s/optional-key :user) s/Str
-         (s/optional-key :password) s/Str}))
+(defmulti uri-types :protocol)
+(defmethod uri-types ::free
+  [_]
+  ::free-uri-description)
+(defmethod uri-types ::ram
+  [_]
+  ::in-memory-uri-description)
+(defmethod uri-types ::sql
+  [_]
+  ::sql-uri-description)
+(s/def ::possible-uri-descriptions (s/multi-spec uri-types :protocol))
 
-(defn possible-uri-descriptions
-  []
-  (let [protocol-p (fn [expected actual]
-                     (= expected (:protocol actual)))]
-    (s/conditional (partial protocol-p :free) (free-uri-description)
-                   (partial protocol-p :ram) (in-memory-uri-description)
-                   (partial protocol-p :sql) (sql-uri-description))))
-
+(defmulti build-connection-string-types :protocol)
 (defmulti build-connection-string :protocol)
+(s/def ::connection-string-builder-types (s/multi-spec build-connection-string-types :protocol))
+
+(defmulti disconnect-type :protocol)
 (defmulti disconnect :protocol)
+(s/def ::disconnect-types (s/multi-spec disconnect-type :protocol))
+
+;;; TODO: Surely these have already been captured somewhere
+
+;; Q: What's a good way to specify that this might have
+;; a length of 2-4?
+;; Actually, this is defined to be 1+ clauses, which can be lots and lots of things.
+;; So this over-simplification has been broken from the beginning.
+(s/def ::where-clause (s/or :ea (s/tuple  symbol? symbol?)
+                            :eav (s/tuple symbol? symbol? symbol?)
+                            :eavt (s/tuple symbol? symbol? symbol? symbol?)))
+
+(s/def ::find (s/coll-of symbol?))
+(s/def ::where ::where-clause)
+(s/def ::in (s/coll-of symbol?))
+(s/def ::datomic-query (s/keys :req-un [::find ::where]
+                               :opt-un ::in))
+
+;; Q: Do I need to create a db namespace for this?
+(s/def :db/id (s/or :id #(instance? DbId %)
+                    :keyword keyword?))
+(s/def ::base-transaction (s/keys :req [:db/id]))
+
+;; Really just a bunch of attribute/value pairs
+(s/def ::upsert-transaction
+  (s/merge ::base-transaction
+           (s/map-of keyword? any?)))
+
+;;; Q: What's a good way to represent these?
+;; UpsertTransactions get translated into these,
+;; which belies the name.
+;; Note that retractions cannot be represented as a map
+;; Q: Is this really supposed to be a coll-of?
+(s/def ::retract-txn (s/tuple #{:db/add :db/retract}
+                              #{integer? keyword?}
+                              any?))
+
+(s/def ::db-before #(instance? Db %))
+(s/def ::db-after #(instance? Db %))
+(s/def ::tx-data (s/coll-of #(instance? Datom ?)))
+(s/def ::temp-ids (s/map-of integer? integer?))
+(s/def ::transaction-result (s/keys :req-un [::db-before ::db-after ::tx-data ::temp-ids]))
+
+(s/def ::description ::possible-uri-descriptions)
+(s/def ::connection-string string?)
+(s/def ::url (s/keys :req-un [::description ::connection-string]))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; Internal
+
+;;; Printing helpers.
+;;; TODO: These really belong in their own namespace.
+;;; Or, at least, not in this one
+;;; Q: Should they really be calling print??
+
+(defmethod io.aviso.exception/exception-dispatch URL
+  [url]
+  (print "<#URL" (:connection-string url) ">"))
+
+(defmethod io.aviso.exception/exception-dispatch DbId
+  [db-id]
+  ;; This is super cheesy. But I need something to keep
+  ;; my error reporting about it from throwing another exception
+  (print "<#DbId" (str db-id) ">"))
+
+(defmethod io.aviso.exception/exception-dispatch schema.core.Either
+  [either]
+  ;; It's tough to fathom that this doesn't have a good default printer
+  (print "(schema.core/either" (:schemas either) ")"))
+
+(defmethod build-connection-string-types ::free
+  [_]
+  ::free-uri-description)
+(defmethod build-connection-string ::free
+  [{:keys [db-name host port]
+    :or {host "localhost"
+         ;; TODO: Verify that this is the default
+         port 4334}}]
+  (str "datomic:free://" host ":" port "/" db-name))
+
+(defmethod build-connection-string-type ::ram
+  [_]
+  ::in-memory-uri-description)
+(defmethod build-connection-string :ram
+  [{:keys [db-name] :as args}]
+  (when-not db-name
+    (throw (ex-info "Missing database name" args)))
+  (str "datomic:mem://" db-name))
+
+(s/fdef sql-driver
+        :args (s/cat :driver-key ::sql-drivers)
+        :ret string?)
+(defn sql-driver
+  "Return the string representation of a JDBC driver"
+  [driver-key]
+  ;; Q: ms?
+  ;; Note that this is really much more interesting.
+  (let [ms {:postgres "postgresql"}]
+    (ms driver-key)))
+
+(defmethod build-connection-string-types :sql
+  [_]
+  ::sql-uri-description)
+(defmethod build-connection-string :sql
+  [{:keys [db-name port driver user password server]
+    :or {port 5432
+         user "datomic"
+         password "datomic"
+         server "localhost"}
+    :as descr}]
+  (when-not driver
+    (throw (ex-info "missing-driver" {:description descr})))
+  ;; Next construct is weird because I've shadowed a builtin
+  (str "datomic:sql://" db-name "?jdbc:" (sql-driver driver)
+       "://" server ":" port "/datomic?user="
+       user "&password=" password))
+
+(defmethod disconnect-type :ram
+  [_]
+  ::in-memory-uri-description)
+(defmethod disconnect :ram
+  [descr]
+  ;; We really don't want to keep a reference around to these
+  (let [cxn-str (build-connection-string descr)]
+    (d/delete-database cxn-str)))
+
+(s/fdef general-disconnect
+        :args ::possible-uri-descriptions
+        ;; Q: What does this return?
+        :ret any?)
+(defn general-disconnect
+  "Generally don't want to delete the database
+
+  This should be done when the entire process loses interest
+  in the connection.
+  Its results are async.
+
+I'm mainly including this on the theory that I might want to switch
+to a different connection during a reset, and multiple connections
+really aren't legal (and probably won't work).
+
+Q: Are those assumptions about multiple connections still true?"
+  [descr]
+  (-> descr build-connection-string d/connect d/release))
+
+(defmethod disconnect-type :sql
+  [_]
+  ;; The original schema for this specified possible-uri-descriptions.
+  ;; Q: Why that instead of this?
+  ::sql-uri-description)
+(defmethod disconnect :sql
+  [descr]
+  (general-disconnect descr))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; Component
 
 ;;; TODO: Come up with a better/less contentious name
-(s/defrecord URL [description :- (possible-uri-descriptions)
-                  connection-string :- s/Str]
+(defrecord URL [description
+                connection-string]
   component/Lifecycle
   (start
    [this]
@@ -82,138 +240,37 @@ if it doesn't already exast and cache the connection"
    ;; from the mailing list
    (assoc this :connection-string nil)))
 
-;;; Printing helpers.
-;;; TODO: These really belong in their own namespace.
-;;; Or, at least, not in this one
-;;; Q: Should they really be calling print??
-
-(defmethod io.aviso.exception/exception-dispatch URL
-  [url]
-  (print "<#URL" (:connection-string url) ">"))
-
-(defmethod io.aviso.exception/exception-dispatch DbId
-  [db-id]
-  ;; This is super cheesy. But I need something to keep
-  ;; my error reporting about it from throwing another exception
-  (print "<#DbId" (str db-id) ">"))
-
-(defmethod io.aviso.exception/exception-dispatch schema.core.Either
-  [either]
-  ;; It's tough to fathom that this doesn't have a good default printer
-  (print "(schema.core/either" (:schemas either) ")"))
-
-;;; TODO: Surely these have already been captured somewhere
-
-;; Q: What's a good way to specify that this might have
-;; a length of 2-4?
-(defn where-clause []
-  [s/Symbol])
-
-(defn datomic-query []
-  {:find [s/Symbol]
-   (s/optional-key :in) [s/Symbol]
-   :where [[(where-clause)]]})
-
-(defn BaseTransaction []
-  {:db/id (s/either DbId s/Keyword)})
-
-;; Really just a bunch of attribute/value pairs
-(defn UpsertTransaction []
-  (into (BaseTransaction) {s/Keyword s/Any}))
-
-;;; Q: What's a good way to represent these?
-;; UpsertTransactions get translated into these.
-;; Note that retractions cannot be represented as a map
-(defn RetractTxn []
-  [(s/one (s/enum :db/add :db/retract) "Action")
-   ;; s/either is deprecated
-   ;; TODO: Switch to s/conditional
-   (s/one (s/either s/Int s/Keyword) "Which entity")
-   s/Any])
-
-(defn TransactionResult []
-  {:db-before Db
-   :db-after Db
-   :tx-data [Datom]
-   :temp-ids {s/Int s/Int}})
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;; Internal
-
-(s/defmethod build-connection-string :free :- s/Str
-  [{:keys [db-name host port]
-    :or {host "localhost"
-         ;; TODO: Verify that this is the default
-         port 4334}}]
-  (str "datomic:free://" host ":" port "/" db-name))
-
-(s/defmethod build-connection-string :ram :- s/Str
-  [{:keys [db-name] :as args}]
-  (when-not db-name
-    (throw (ex-info "Missing database name" args)))
-  (str "datomic:mem://" db-name))
-
-(s/defn sql-driver :- s/Str
-  "Return the string representation of a JDBC driver"
-  [driver-key :- sql-drivers]
-  (let [ms {:postgres "postgresql"}]
-    (ms driver-key)))
-
-(s/defmethod build-connection-string :sql :- s/Str
-  [{:keys [db-name port driver user password server]
-    :or {port 5432
-         user "datomic"
-         password "datomic"
-         server "localhost"}
-    :as descr} :- (sql-uri-description)]
-  (when-not driver
-    (raise :missing-driver))
-  ;; Next construct is weird because I've shadowed a builtin
-  (str "datomic:sql://" db-name "?jdbc:" (sql-driver driver)
-       "://" server ":" port "/datomic?user="
-       user "&password=" password))
-
-(s/defmethod disconnect :ram
-  [descr :- (in-memory-uri-description)]
-  ;; We really don't want to keep a reference around to these
-  (let [cxn-str (build-connection-string descr)]
-    (d/delete-database cxn-str)))
-
-(s/defn general-disconnect
-  "Generally don't want to delete the database
-
-  This should be done when the entire process loses interest
-  in the connection.
-  Its results are async.
-
-I'm mainly including this on the theory that I might want to switch
-to a different connection during a reset, and multiple connections
-really aren't legal (and probably won't work)."
-  [descr :- (possible-uri-descriptions)]
-  (-> descr build-connection-string d/connect d/release))
-
-(s/defmethod disconnect :sql
-  [descr :- (possible-uri-descriptions)]
-  (general-disconnect descr))
-
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; Public
 
-(s/defn uri-ctor :- URL
-  [description :- {:description (possible-uri-descriptions)}]
+(s/fdef uri-ctor
+        :args (s/cat :description (s/map-of #(= :description %) ::possible-uri-descriptions))
+        :ret ::url)
+(defn uri-ctor
+  [description]
   (map->URL description))
 
-(s/defn q :- [s/Any]
+(s/fdef q
+        :args (s/cat :query ::datomic-query
+                     :uri ::connection-string)
+        ;; Q: What do I know about the actual return results?
+        ;; A: Well, it's actually a seq of seqs. Where the inner seq matches the query's :find
+        ;; clause. That seems worth encoding formally.
+        :ret (s/coll-of any?))
+(defn q
   "Convenience function for querying the database.
 Probably shouldn't actually use very often.
 
 In general, we should probably be running queries against database values
 using d/q. But this approach does save some typing"
-  [query :- datomic-query
-   uri :- s/Str]
+  [query uri]
   (d/q query (-> uri d/connect d/db)))
 
-(s/defn pretend-upsert! :- TransactionResult
+(s/fdef pretend-upsert!
+        :args (s/cat :uri ::connection-string
+                     :txns (s/coll-of ::upsert-transaction))
+        :ret ::transaction-result)
+(defn pretend-upsert!
   "Re-bind upsert! to this for experimentation
 or unit testing
 
@@ -221,14 +278,16 @@ Then again, that would mean making it dynamic, which
 seems like a bad idea. If nothing else, I think it
 has noticeable performance impact because of the
 var lookup"
-  [uri :- s/Str
-   txns :- [UpsertTransaction]]
+  [uri txns]
   (let [conn (d/connect uri)
         database-value (d/db conn)]
     (d/with database-value txns)))
 
-(s/defn upsert! :- TransactionResult
-  [uri :- s/Str
-   txns :- [UpsertTransaction]]
+(s/fdef upsert!
+        :args (s/cat :uri ::connection-string
+                     :txns (s/coll-of ::upsert-transaction))
+        :ret ::tranaction-result)
+(defn upsert!
+  [uri txns]
   (let [conn (d/connect uri)]
     @(d/transact conn txns)))
